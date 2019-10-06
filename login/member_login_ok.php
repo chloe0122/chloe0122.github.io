b704604b374e4a7d882267af47430b9977cddd13
chloesso <chloesso@naver.com>
2019-10-06 21:57:53 +0900

    update login

--- login/member_login_ok.php
+++ login/member_login_ok.php
@@ -1,20 +1,19 @@
 <?php
-	if(!isset($_POST['user_id']) || 
-	!isset($_POST['user_pw'])) exit;
-
-	$user_id = $_POST['user_id'];
-	$user_pw = $_POST['user_pw'];
-
-	$members = array(
-		'korean'=>array('pw'=>'12345', 
-		'name'=>'박부장'),
-		'seoul'=>array('pw'=>'56789', 
-		'name'=>'홍대리')
-	);
-
-	if(isset($members[$user_id]) &&  
-	$members[$user_id]['pw'] == $user_pw) {
-		echo '{"user_id":"'.$user_id.'", 
-		"user_name": "'.$members[$user_id]['name'].'"}';
-	}
+if(!isset($_POST['user_id']) || !isset($_POST['user_pw'])) exit;
+$user_id = $_POST['user_id'];
+$user_pw = $_POST['user_pw'];
+$members = array('user1'=>array('pw'=>'pw1', 'name'=>'한놈'),
+        'user2'=>array('pw'=>'pw2', 'name'=>'두시기'),
+        'user3'=>array('pw'=>'pw3', 'name'=>'석삼'));
+ 
+if(!isset($members[$user_id])) {
+        echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
+        exit;
+}
+if($members[$user_id]['pw'] != $user_pw) {
+        echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다.');history.back();</script>";
+        exit;
+}
+setcookie('user_id',$user_id,time()+3600,'/');
+setcookie('user_name',$members[$user_id]['name'],time()+3600,'/');
 ?>
