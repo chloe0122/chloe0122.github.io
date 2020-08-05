6e7493bc5b490a8f0575626123e457e62dd85e93
chloe0122 <kyung0446@gmail.com>
2020-08-05 21:23:23 +0900

    css update

--- aduino/script.js
+++ aduino/script.js
@@ -1,14 +1,4 @@
 function fnLedClick(){            
     const ledElem = document.querySelector(".led");
-    if( ledElem.getAttribute("value") == "on" ){
-        ledElem.style.background = "gray";
-        ledElem.innerHTML = "LED<br />OFF ";
-        ledElem.setAttribute("value","off");
-    }
-    else{
-        ledElem.style.background = "#fff";
-        ledElem.innerHTML = "LED<br />ON ";
-        ledElem.setAttribute("value","on");
-    }
-    
+    ledElem.classList.toggle("on");    
 }
